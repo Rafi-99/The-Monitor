603d427e3b969d87d32e5795c86fc1dfbcb252c4
rafix004 <rafix004@d.umn.edu>
2020-09-20 14:34:27 -0500

    Changed to Activity.playing()

--- src/main/java/bot/driver/Monitor.java
+++ src/main/java/bot/driver/Monitor.java
@@ -25,6 +25,6 @@ public class Monitor {
          .build()
          .awaitReady();
 
-         myBot.getPresence().setActivity(Activity.listening(Monitor.prefix + "botInfo"));
+         myBot.getPresence().setActivity(Activity.playing(Monitor.prefix + "botInfo"));
     }
 }
