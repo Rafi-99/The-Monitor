fc52036b0ae67ccad89a481ade4aeb4fb2eb9489
rafix004 <rafix004@d.umn.edu>
2020-11-06 13:49:07 -0600

    Reformatted file

--- src/main/java/bot/music/AudioPlayerSendHandler.java
+++ src/main/java/bot/music/AudioPlayerSendHandler.java
@@ -6,39 +6,35 @@ import net.dv8tion.jda.api.audio.AudioSendHandler;
 
 import java.nio.ByteBuffer;
 
-/**
+/*
  * This is a wrapper around AudioPlayer which makes it behave as an AudioSendHandler for JDA. As JDA calls canProvide
  * before every call to provide20MsAudio(), we pull the frame in canProvide() and use the frame we already pulled in
  * provide20MsAudio().
  */
 public class AudioPlayerSendHandler implements AudioSendHandler {
-  private final AudioPlayer audioPlayer;
-  private final ByteBuffer buffer;
-  private final MutableAudioFrame frame;
+     private final AudioPlayer audioPlayer;
+     private final ByteBuffer buffer;
+     private final MutableAudioFrame frame;
 
-  /**
-   * @param audioPlayer Audio player to wrap.
-   */
-  public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
-    this.audioPlayer = audioPlayer;
-    this.buffer = ByteBuffer.allocate(1024);
-    this.frame = new MutableAudioFrame();
-    this.frame.setBuffer(buffer);
-  }
+     // @param audioPlayer Audio player to wrap
+     public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
+          this.audioPlayer = audioPlayer;
+          this.buffer = ByteBuffer.allocate(1024);
+          this.frame = new MutableAudioFrame();
+          this.frame.setBuffer(buffer);
+     }
 
-  @Override
-  public boolean canProvide() {
-    // returns true if audio was provided
-    return this.audioPlayer.provide(this.frame);
-  }
-
-  @Override
-  public ByteBuffer provide20MsAudio() {
-    return this.buffer.flip();
-  }
-
-  @Override
-  public boolean isOpus() {
-    return true;
-  }
+     // returns true if audio was provided
+     @Override
+     public boolean canProvide() {
+          return this.audioPlayer.provide(this.frame);
+     }
+     @Override
+     public ByteBuffer provide20MsAudio() {
+          return this.buffer.flip();
+     }
+     @Override
+     public boolean isOpus() {
+          return true;
+     }
 }
