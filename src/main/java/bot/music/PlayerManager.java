2f592139391e5cbab7668342fdee1f510b50b494
rafix004 <rafix004@d.umn.edu>
2020-11-06 21:32:42 -0600

    Reformatted file

--- src/main/java/bot/music/PlayerManager.java
+++ src/main/java/bot/music/PlayerManager.java
@@ -15,58 +15,59 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PlayerManager {
-    private static PlayerManager INSTANCE;
-    private final AudioPlayerManager playerManager;
-    private final Map<Long, GuildMusicManager> musicManagers;
+     private static PlayerManager INSTANCE;
+     private final AudioPlayerManager playerManager;
+     private final Map<Long, GuildMusicManager> musicManagers;
 
-    private PlayerManager() {
-         this.musicManagers = new HashMap<>();
-         this.playerManager = new DefaultAudioPlayerManager();
-         AudioSourceManagers.registerRemoteSources(playerManager);
-         AudioSourceManagers.registerLocalSource(playerManager);
+     private PlayerManager() {
+          this.musicManagers = new HashMap<>();
+          this.playerManager = new DefaultAudioPlayerManager();
+          AudioSourceManagers.registerRemoteSources(playerManager);
+          AudioSourceManagers.registerLocalSource(playerManager);
      }
 
-    public GuildMusicManager getMusicManager(Guild guild) {
-         return this.musicManagers.computeIfAbsent(guild.getIdLong(), (guildId) -> {
-              final GuildMusicManager guildMusicManager = new GuildMusicManager(this.playerManager);
-              guild.getAudioManager().setSendingHandler(guildMusicManager.getSendHandler());
-              return guildMusicManager;
-         });
-    }
-    
-    public void loadAndPlay(TextChannel channel, String trackUrl) {
-         final GuildMusicManager musicManager = this.getMusicManager(channel.getGuild());
+     public GuildMusicManager getMusicManager(Guild guild) {
+          return this.musicManagers.computeIfAbsent(guild.getIdLong(), (guildId) -> {
+               final GuildMusicManager guildMusicManager = new GuildMusicManager(this.playerManager);
+               guild.getAudioManager().setSendingHandler(guildMusicManager.getSendHandler());
+               return guildMusicManager;
+          });
+     } 
 
-         this.playerManager.loadItemOrdered(musicManager, trackUrl, new AudioLoadResultHandler(){
-              @Override
-              public void trackLoaded(AudioTrack track) {
+     public void loadAndPlay(TextChannel channel, String trackUrl) {
+          final GuildMusicManager musicManager = this.getMusicManager(channel.getGuild());
+          this.playerManager.loadItemOrdered(musicManager, trackUrl, new AudioLoadResultHandler() {
+               @Override
+               public void trackLoaded(AudioTrack track) {
                     musicManager.scheduler.queue(track);
+                    channel.sendTyping().queue();
                     channel.sendMessage("Adding "+ track.getInfo().title + " to the queue.").queue();
-              }
-              @Override
-              public void playlistLoaded(AudioPlaylist playlist) {
-                   for (int i = 0; i < playlist.getTracks().size(); i++) {
-                        musicManager.scheduler.queue(playlist.getTracks().get(i));
-                   }
-                   channel.sendMessage("Adding " + playlist.getTracks().size() + " tracks from "+ playlist.getName() + " to the queue.").queue();
-
-              }
-              @Override
-              public void noMatches() {
+               }
+               @Override
+               public void playlistLoaded(AudioPlaylist playlist) {
+                    for (int i = 0; i < playlist.getTracks().size(); i++) {
+                         musicManager.scheduler.queue(playlist.getTracks().get(i));
+                    }
+                    channel.sendTyping().queue();
+                    channel.sendMessage("Adding " + playlist.getTracks().size() + " tracks from "+ playlist.getName() + " to the queue.").queue();
+               }
+               @Override
+               public void noMatches() {
+                    channel.sendTyping().queue();
                     channel.sendMessage("Sorry, no matches were found from your request.").queue();
-              }
-              @Override
-              public void loadFailed(FriendlyException exception) {
-                    channel.sendMessage("Invalid format.").queue();
-              }
-         });
-    }
+               }
+               @Override
+               public void loadFailed(FriendlyException exception) {
+                    channel.sendTyping().queue();
+                    channel.sendMessage("Sorry, failed to load your request. Invalid Format.").queue();
+               }
+          });
+     }
 
-    public static PlayerManager getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new PlayerManager();
-        }
-
-        return INSTANCE;
-    }
+     public static PlayerManager getInstance() {
+          if(INSTANCE == null) {
+               INSTANCE = new PlayerManager();
+          }
+          return INSTANCE;
+     }
 }
