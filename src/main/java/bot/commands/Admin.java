c23c1d5749116d1ba8a8a780ed715561eed9fa37
Rafi-99 <rafi.md.2018@gmail.com>
2020-12-15 15:56:54 -0600

    Minor changes and refactoring

--- src/main/java/bot/commands/Admin.java
+++ src/main/java/bot/commands/Admin.java
@@ -2,6 +2,7 @@ package bot.commands;
 
 import bot.driver.Monitor;
 
+import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -17,24 +18,23 @@ public class Admin extends ListenerAdapter {
 
      @Override
      public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+          
           String [] admin = event.getMessage().getContentRaw().split("\\s+"); 
 
           if(admin[0].equalsIgnoreCase(Monitor.prefix + "test") && event.getAuthor().getId().equals("398215411998654466") && admin.length == 1) {
                ScheduledExecutorService test = Executors.newSingleThreadScheduledExecutor();
-               test.scheduleAtFixedRate(new Runnable() {
-                    @Override
-                    public void run() {
-                         event.getChannel().sendMessage("Poggers!").queue();
-                         if(stop) {
-                              event.getChannel().sendTyping().queue();
-                              event.getChannel().sendMessage("Ending spam now...").queue();
-                              event.getChannel().sendMessage("Ended.").queue();
-                              test.shutdownNow();
-                              stop = false;
-                         }
+               test.scheduleAtFixedRate(() -> {
+                    event.getChannel().sendMessage("Poggers!").queue();
+                    if(stop) {
+                         event.getChannel().sendTyping().queue();
+                         event.getChannel().sendMessage("Ending spam now...").queue();
+                         event.getChannel().sendMessage("Ended.").queue();
+                         test.shutdownNow();
+                         stop = false;
                     }
                }, 0, 1, TimeUnit.SECONDS);
           } 
+
           else if(admin[0].equalsIgnoreCase(Monitor.prefix + "stopTest") && event.getAuthor().getId().equals("398215411998654466") && admin.length == 1) {
                stop = true;
           }
@@ -77,7 +77,7 @@ public class Admin extends ListenerAdapter {
           // Automated link spam deletion in Goddess's Parthenon 
           else if(event.getMessage().getContentRaw().contains("https://") || event.getMessage().getContentRaw().contains("http://")) {
                Role staff = event.getGuild().getRoleById("710398399085805599");
-               if(event.getChannel().getId().equals("709259200651591747") && !event.getMember().getRoles().contains(staff)) {
+               if(event.getChannel().getId().equals("709259200651591747") && !Objects.requireNonNull(event.getMember()).getRoles().contains(staff)) {
                     event.getMessage().delete().queue(); 
                }
           }
