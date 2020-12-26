7f160635b87e203a4048f421ace9e8608ebff963
Rafi-99 <rafi.md.2018@gmail.com>
2020-12-26 16:01:33 -0600

    Formatting changes

--- src/main/java/bot/music/AudioPlayerSendHandler.java
+++ src/main/java/bot/music/AudioPlayerSendHandler.java
@@ -1,10 +1,11 @@
 package bot.music;
 
+import java.nio.ByteBuffer;
+
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame;
-import net.dv8tion.jda.api.audio.AudioSendHandler;
 
-import java.nio.ByteBuffer;
+import net.dv8tion.jda.api.audio.AudioSendHandler;
 
 /*
  * This is a wrapper around AudioPlayer which makes it behave as an AudioSendHandler for JDA. As JDA calls canProvide
@@ -12,11 +13,14 @@ import java.nio.ByteBuffer;
  * provide20MsAudio().
  */
 public class AudioPlayerSendHandler implements AudioSendHandler {
+
      private final AudioPlayer audioPlayer;
      private final ByteBuffer buffer;
      private final MutableAudioFrame frame;
 
-     // @param audioPlayer Audio player to wrap
+     /* 
+      * @param audioPlayer: Audio player to wrap.
+      */
      public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
           this.audioPlayer = audioPlayer;
           this.buffer = ByteBuffer.allocate(1024);
@@ -24,15 +28,19 @@ public class AudioPlayerSendHandler implements AudioSendHandler {
           this.frame.setBuffer(buffer);
      }
 
-     // returns true if audio was provided
+     /* 
+      * Returns true if audio was provided.
+      */
      @Override
      public boolean canProvide() {
           return this.audioPlayer.provide(this.frame);
      }
+
      @Override
      public ByteBuffer provide20MsAudio() {
           return this.buffer.flip();
      }
+     
      @Override
      public boolean isOpus() {
           return true;
