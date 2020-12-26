e3ab3927f691e186ff3c142ab2970e13a29e4047
Rafi-99 <rafi.md.2018@gmail.com>
2020-12-26 16:06:37 -0600

    Formatting changes

--- src/main/java/bot/driver/Monitor.java
+++ src/main/java/bot/driver/Monitor.java
@@ -12,11 +12,13 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
-public class Monitor {	
+public class Monitor {
+
     public static JDA myBot;
     public static String prefix = "m!";
     
     public static void main(String[] args) throws LoginException, InterruptedException {
+
         myBot = JDABuilder.createDefault(System.getenv("BOT_TOKEN"))
         .enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_VOICE_STATES)
         .setMemberCachePolicy(MemberCachePolicy.ALL)
