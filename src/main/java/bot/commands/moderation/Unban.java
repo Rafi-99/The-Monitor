cab5aaaf73940b97e8495d0dbca568db98e0ac42
Rafi-99 <rafi.md.2018@gmail.com>
2021-01-16 14:44:08 -0600

    Completely redone

--- src/main/java/bot/commands/moderation/Unban.java
+++ src/main/java/bot/commands/moderation/Unban.java
@@ -5,8 +5,15 @@ import bot.commands.CommandInterface;
 import bot.driver.Monitor;
 import bot.handlers.event.ModerationUtility;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.Guild.Ban;
+import net.dv8tion.jda.api.requests.RestAction;
 
 public class Unban implements CommandInterface {
 
@@ -27,22 +34,58 @@ public class Unban implements CommandInterface {
             else {
 
                 try {
-                    String [] format = c.getCommandParameters().toArray(new String[0]);
-                    String memberID = format[0].replace("<@!", "").replace("<@", "").replace(">", "");
-                    c.getGuild().unban(memberID).queue();
-                    EmbedBuilder unbanSuccess = new EmbedBuilder();
-                    unbanSuccess.setColor(0x05055e);
-                    unbanSuccess.setTitle("✅ Success! ✅");
-                    unbanSuccess.setDescription("<@" + memberID + ">" + " has been unbanned successfully!");
-                    unbanSuccess.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
-                    c.getChannel().sendTyping().queue();
-                    c.getChannel().sendMessage(unbanSuccess.build()).queue();
-                    unbanSuccess.clear();
-                } catch (Exception e) {
+                    RestAction<User> removeBanEvent = c.getJDA().retrieveUserById(c.getCommandParameters().get(0).replace("<@!", "").replace("<@", "").replace(">", ""));
+                    removeBanEvent.queue((user) -> {
+
+                        if(user != null) {
+                            Consumer<? super List<Ban>> banListConsumer = (b) -> {
+
+                                List<User> banIDList = new ArrayList<>(b.size());
+                                for(int i=0; i < b.size(); i++) {
+                                    banIDList.add(i, b.get(i).getUser());
+                                }
+
+                                if(banIDList.contains(user)) {
+                                    c.getGuild().unban(user).queue();
+                                    EmbedBuilder unbanSuccess = new EmbedBuilder();
+                                    unbanSuccess.setColor(0x05055e);
+                                    unbanSuccess.setTitle("✅ Success! ✅");
+                                    unbanSuccess.setDescription(user.getAsMention() + " has been unbanned successfully!");
+                                    unbanSuccess.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
+                                    c.getChannel().sendTyping().queue();
+                                    c.getChannel().sendMessage(unbanSuccess.build()).queue();
+                                    unbanSuccess.clear();
+                                }
+                                else {
+                                    EmbedBuilder unbanError = new EmbedBuilder();
+                                    unbanError.setColor(0x05055e);
+                                    unbanError.setTitle("❌ Failed to Unban ❌");
+                                    unbanError.setDescription("Cannot unban users that aren't on the banlist.");
+                                    unbanError.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
+                                    c.getChannel().sendTyping().queue();
+                                    c.getChannel().sendMessage(unbanError.build()).queue();
+                                    unbanError.clear();
+                                }
+                            };
+                            //Retrieves the ban list then runs the code inside the consumer to unban the user.
+                            c.getGuild().retrieveBanList().queue(banListConsumer);
+                        }
+                    }, (error) -> {
+                        EmbedBuilder unbanError = new EmbedBuilder();
+                        unbanError.setColor(0x05055e);
+                        unbanError.setTitle("❌ Failed to Unban ❌");
+                        unbanError.setDescription("Invalid user. Please try executing the command again with a valid user mention or user ID.");
+                        unbanError.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
+                        c.getChannel().sendTyping().queue();
+                        c.getChannel().sendMessage(unbanError.build()).queue();
+                        unbanError.clear();
+                    });
+                } 
+                catch (Exception e) {
                     EmbedBuilder unbanError = new EmbedBuilder();
                     unbanError.setColor(0x05055e);
-                    unbanError.setTitle("❌ Invalid Argument ❌");
-                    unbanError.setDescription("Users that are no longer in a guild cannot be mentioned. Please try executing the command again with a valid user mention or user ID.");
+                    unbanError.setTitle("❌ Failed to Unban ❌");
+                    unbanError.setDescription("Bad format. Please try executing the command again with a valid user mention or user ID.");
                     unbanError.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
                     c.getChannel().sendTyping().queue();
                     c.getChannel().sendMessage(unbanError.build()).queue();
