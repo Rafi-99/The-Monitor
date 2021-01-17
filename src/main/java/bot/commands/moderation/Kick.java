a4e6c6ad9ca79721c707b27ba268151ed5463b86
Rafi-99 <rafi.md.2018@gmail.com>
2021-01-17 17:00:38 -0600

    Refactored

--- src/main/java/bot/commands/moderation/Kick.java
+++ src/main/java/bot/commands/moderation/Kick.java
@@ -3,9 +3,9 @@ package bot.commands.moderation;
 import bot.commands.CommandContext;
 import bot.commands.CommandInterface;
 import bot.driver.Monitor;
+import bot.handlers.event.FunUtility;
 import bot.handlers.event.ModerationUtility;
 
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.requests.RestAction;
@@ -15,16 +15,8 @@ public class Kick implements CommandInterface {
     @Override
     public void handle(CommandContext c) {
         if(c.getMember().hasPermission(Permission.KICK_MEMBERS)) {
-
             if(c.getCommandParameters().size() < 1) {
-                EmbedBuilder kick = new EmbedBuilder();
-                kick.setColor(0x05055e);
-                kick.setTitle("Kick Command Usage");
-                kick.setDescription("Usage: "+ Monitor.prefix +"kick [user mention or ID]");
-                kick.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
-                c.getChannel().sendTyping().queue();
-                c.getChannel().sendMessage(kick.build()).queue();
-                kick.clear();
+                FunUtility.setEmbed(c.getEvent(), "Kick Command Usage", "Usage: "+ Monitor.prefix +"kick [user mention or ID]");
             }
             else {
                 try {
@@ -32,42 +24,18 @@ public class Kick implements CommandInterface {
                     kickEvent.queue((user) -> {
                         if(c.getEvent().getGuild().getMemberById(user.getId()) != null) {
                             c.getEvent().getGuild().kick(user).queue();
-                            EmbedBuilder kicked = new EmbedBuilder();
-                            kicked.setColor(0x05055e);
-                            kicked.setTitle("✅ Success! ✅");
-                            kicked.setDescription(user.getAsMention() +" has been kicked successfully!");
-                            kicked.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
-                            c.getChannel().sendTyping().queue();
-                            c.getChannel().sendMessage(kicked.build()).queue();
-                            kicked.clear();
+                            FunUtility.setEmbed(c.getEvent(), "✅ Success! ✅", user.getAsMention() +" has been kicked successfully!");
                         }
-                    }, (error) -> {
-                        EmbedBuilder kickError = new EmbedBuilder();
-                        kickError.setColor(0x05055e);
-                        kickError.setTitle("❌ Failed to Kick ❌");
-                        kickError.setDescription("Invalid user. Users not in the guild can't be kicked. Please try executing the command again with a valid user mention or user ID.");
-                        kickError.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
-                        c.getChannel().sendTyping().queue();
-                        c.getChannel().sendMessage(kickError.build()).queue();
-                        kickError.clear();
-                    });
+                    }, (error) -> FunUtility.setEmbed(c.getEvent(), "❌ Failed to Kick ❌", "Invalid user. Users not in the guild can't be kicked. Please try executing the command again with a valid user mention or user ID."));
                 } 
                 catch (Exception e) {
-                    EmbedBuilder kickError = new EmbedBuilder();
-                    kickError.setColor(0x05055e);
-                    kickError.setTitle("❌ Failed to Kick ❌");
-                    kickError.setDescription("Bad format. Please try executing the command again with a valid user mention or user ID.");
-                    kickError.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
-                    c.getChannel().sendTyping().queue();
-                    c.getChannel().sendMessage(kickError.build()).queue();
-                    kickError.clear();
+                    FunUtility.setEmbed(c.getEvent(), "❌ Failed to Kick ❌", "Bad format. Please try executing the command again with a valid user mention or user ID.");
                 }
             }
         }
         else {
             ModerationUtility.accessDenied(c.getEvent());
         }
-
     }
 
     @Override
