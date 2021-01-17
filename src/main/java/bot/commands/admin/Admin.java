449abdcd7490f16fc1f664b582a0cda138f8e50c
Rafi-99 <rafi.md.2018@gmail.com>
2021-01-17 16:03:45 -0600

    Reverted back to original since embed was not being sent in DM

--- src/main/java/bot/commands/admin/Admin.java
+++ src/main/java/bot/commands/admin/Admin.java
@@ -2,7 +2,9 @@ package bot.commands.admin;
 
 import bot.commands.CommandContext;
 import bot.commands.CommandInterface;
-import bot.handlers.event.FunUtility;
+import bot.driver.Monitor;
+
+import net.dv8tion.jda.api.EmbedBuilder;
 
 public class Admin implements CommandInterface {
 
@@ -11,7 +13,17 @@ public class Admin implements CommandInterface {
         if(c.getCommandParameters().isEmpty() && c.getMember().getId().equals("398215411998654466")) {
             c.getChannel().sendTyping().queue();
             c.getChannel().sendMessage("The information has been sent to your DM!").queue();
-            c.getAuthor().openPrivateChannel().queue(privateChannel -> FunUtility.setEmbed(c.getEvent(), "Admin Tools", "Commands available for your usage: \n```test -s \ntest -t \nadmin \nrestart \nlink \nguilds```"));
+            c.getAuthor().openPrivateChannel().queue(privateChannel -> {
+                EmbedBuilder adminInfo = new EmbedBuilder();
+                adminInfo.setColor(0x05055e);
+                adminInfo.setTitle("Admin Tools");
+                adminInfo.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
+                adminInfo.setDescription("Commands available for your usage: \n```test -s \ntest -t \nadmin \nrestart \nlink \nguilds```");
+                privateChannel.sendTyping().queue();
+                privateChannel.sendMessage(adminInfo.build()).queue();
+                adminInfo.clear();
+                privateChannel.close().queue();
+            });
         }
     }
 
