aa67021bdff622bf6bfb10e99ef3f7b54f9b5400
Rafi-99 <rafi.md.2018@gmail.com>
2021-01-17 16:30:37 -0600

    Refactored

--- src/main/java/bot/handlers/event/ModerationUtility.java
+++ src/main/java/bot/handlers/event/ModerationUtility.java
@@ -1,20 +1,10 @@
 package bot.handlers.event;
 
-import bot.driver.Monitor;
-
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class ModerationUtility {
 
     public static void accessDenied(GuildMessageReceivedEvent event) {
-        EmbedBuilder denied = new EmbedBuilder();
-        denied.setColor(0x05055e);
-        denied.setTitle("❌ Access Denied! ❌");
-        denied.setDescription("Sorry, you don't have the required permissions to use this command.");
-        denied.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
-        event.getChannel().sendTyping().queue();
-        event.getChannel().sendMessage(denied.build()).queue();
-        denied.clear();
+        FunUtility.setEmbed(event, "❌ Access Denied! ❌", "Sorry, you don't have the required permissions to use this command.");
     }
 }
