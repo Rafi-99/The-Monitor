56aeb159c196b424ad3e241220f19b3bf5968fbc
Rafi-99 <rafi.md.2018@gmail.com>
2021-01-17 17:09:46 -0600

    Refactored

--- src/main/java/bot/commands/moderation/SetPrefix.java
+++ src/main/java/bot/commands/moderation/SetPrefix.java
@@ -3,9 +3,9 @@ package bot.commands.moderation;
 import bot.commands.CommandContext;
 import bot.commands.CommandInterface;
 import bot.driver.Monitor;
+import bot.handlers.event.FunUtility;
 import bot.handlers.event.ModerationUtility;
 
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 
 public class SetPrefix implements CommandInterface {
@@ -16,24 +16,10 @@ public class SetPrefix implements CommandInterface {
 
             if(c.getCommandParameters().size() == 1) {
                 Monitor.prefix = c.getCommandParameters().get(0);
-                EmbedBuilder prefixSet = new EmbedBuilder();
-                prefixSet.setColor(0x05055e);
-                prefixSet.setTitle("✅ Success! ✅");
-                prefixSet.setDescription("The prefix has now been set to " + Monitor.prefix);
-                prefixSet.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
-                c.getChannel().sendTyping().queue();
-                c.getChannel().sendMessage(prefixSet.build()).queue();
-                prefixSet.clear();
+                FunUtility.setEmbed(c.getEvent(), "✅ Success! ✅", "The prefix has now been set to " + Monitor.prefix);
             }
             else {
-                EmbedBuilder prefixInfo = new EmbedBuilder();
-                prefixInfo.setColor(0x05055e);
-                prefixInfo.setTitle("Prefix Command Usage");
-                prefixInfo.setDescription("Usage: " + Monitor.prefix + "setPrefix [prefix]");
-                prefixInfo.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
-                c.getChannel().sendTyping().queue();
-                c.getChannel().sendMessage(prefixInfo.build()).queue();
-                prefixInfo.clear();
+                FunUtility.setEmbed(c.getEvent(), "Prefix Command Usage", "Usage: " + Monitor.prefix + "setPrefix [prefix]");
             }
         }
         else {
