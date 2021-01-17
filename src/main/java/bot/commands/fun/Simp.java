0ecd19b7b795bce394a595280aa136c4a815f435
Rafi-99 <rafi.md.2018@gmail.com>
2021-01-17 15:49:01 -0600

    Refactored

--- src/main/java/bot/commands/fun/Simp.java
+++ src/main/java/bot/commands/fun/Simp.java
@@ -3,22 +3,14 @@ package bot.commands.fun;
 import bot.commands.CommandContext;
 import bot.commands.CommandInterface;
 import bot.driver.Monitor;
-
-import net.dv8tion.jda.api.EmbedBuilder;
+import bot.handlers.event.FunUtility;
 
 public class Simp implements CommandInterface {
     
     @Override
     public void handle(CommandContext c) {
         if(c.getCommandParameters().size() == 1) {
-            EmbedBuilder simp = new EmbedBuilder();
-            simp.setColor(0x05055e);
-            simp.setTitle("Such Wholesome");
-            simp.setFooter("The Monitor ™ | Powered by Java", Monitor.myBot.getSelfUser().getEffectiveAvatarUrl());
-            simp.setDescription(c.getCommandParameters().get(0) + " is " + (int) (Math.random() * 101) + "% simp.");
-            c.getChannel().sendTyping().queue();
-            c.getChannel().sendMessage(simp.build()).queue();
-            simp.clear();
+            FunUtility.setEmbed(c.getEvent(), "Simps be Simping", c.getCommandParameters().get(0) + " is " + (int) (Math.random() * 101) + "% simp.");
         }
         else {
             c.getChannel().sendTyping().queue();
