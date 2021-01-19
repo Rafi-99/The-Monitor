bfac985d7438b36bcbf68683cb4e9acb1563f393
Rafi-99 <rafi.md.2018@gmail.com>
2021-01-19 14:08:50 -0600

    Included code to show updated bot status

--- src/main/java/bot/commands/moderation/SetPrefix.java
+++ src/main/java/bot/commands/moderation/SetPrefix.java
@@ -5,8 +5,8 @@ import bot.commands.CommandInterface;
 import bot.driver.Monitor;
 import bot.handlers.event.FunUtility;
 import bot.handlers.event.ModerationUtility;
-
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Activity;
 
 public class SetPrefix implements CommandInterface {
 
@@ -17,6 +17,7 @@ public class SetPrefix implements CommandInterface {
             if(c.getCommandParameters().size() == 1) {
                 Monitor.prefix = c.getCommandParameters().get(0);
                 FunUtility.setEmbed(c.getEvent(), "✅ Success! ✅", "The prefix has now been set to " + Monitor.prefix);
+                Monitor.myBot.getPresence().setActivity(Activity.playing(Monitor.prefix + "botInfo"));
             }
             else {
                 FunUtility.setEmbed(c.getEvent(), "Prefix Command Usage", "Usage: " + Monitor.prefix + "setPrefix [prefix]");
