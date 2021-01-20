f41e6289c72f51cd31f12868a1d0ca386f0af2a9
Rafi-99 <rafi.md.2018@gmail.com>
2021-01-19 18:56:22 -0600

    Created methods for implementation and removed old code

--- src/main/java/bot/handlers/music/MusicUtility.java
+++ src/main/java/bot/handlers/music/MusicUtility.java
@@ -7,11 +7,17 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.managers.AudioManager;
 
-public class MusicUtility {
+public class MusicUtility  {
+    
+    public static AudioManager getAudioManager(GuildMessageReceivedEvent event) {
+        return event.getGuild().getAudioManager();
+    }
 
-    public static GuildMessageReceivedEvent event;
-    public static AudioManager guildAudioManager = event.getGuild().getAudioManager();
-    public static GuildMusicManager guildMusicManager = PlayerManager.getInstance().getMusicManager(event.getGuild());
-    public static BlockingQueue<AudioTrack> playerQueue = PlayerManager.getInstance().getMusicManager(event.getGuild()).scheduler.getQueue(); 
+    public static GuildMusicManager getMusicManager(GuildMessageReceivedEvent event) {
+        return PlayerManager.getInstance().getMusicManager(event.getGuild());
+    }
 
+    public static BlockingQueue<AudioTrack> getQueue(GuildMessageReceivedEvent event) {
+        return PlayerManager.getInstance().getMusicManager(event.getGuild()).scheduler.getQueue();
+    }
 }
