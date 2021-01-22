c7587c45cace2942ddffe354078ae79df16ea13a
Rafi-99 <rafi.md.2018@gmail.com>
2021-01-21 19:33:44 -0600

    Redesigned command with ability to delete up to 1000 messages

--- src/main/java/bot/commands/moderation/Purge.java
+++ src/main/java/bot/commands/moderation/Purge.java
@@ -6,10 +6,7 @@ import bot.driver.Monitor;
 import bot.handlers.event.FunUtility;
 import bot.handlers.event.ModerationUtility;
 
-import java.util.List;
-
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Message;
 
 public class Purge implements CommandInterface {
 
@@ -17,37 +14,27 @@ public class Purge implements CommandInterface {
     public void handle(CommandContext c) {
         if(c.getMember().hasPermission(Permission.MESSAGE_MANAGE)) {
 
-            if(c.getCommandParameters().size() < 1) {
+            if(c.getCommandParameters().isEmpty()) {
                 FunUtility.setEmbed(c.getEvent(), "Message Deletion Usage", "Usage: " + Monitor.prefix + "purge [# of messages]");
             }
             else {
-                
-                try {
-                    int messageCount = Integer.parseInt(c.getCommandParameters().get(0));
-                    List<Message> messages = c.getChannel().getHistory().retrievePast(messageCount).complete();
-                    c.getChannel().purgeMessages(messages);
-                    /*
-                     * Notifies user if messages have been successfully deleted.
-                     */
-                    FunUtility.setEmbed(c.getEvent(), "✅ Success! ✅", "You have successfully deleted " + messageCount + " messages.");
-                }
-                catch(NumberFormatException n) {
-                    FunUtility.setEmbed(c.getEvent(), "❌ Invalid Argument ❌", "Please enter the number of messages you want to delete.");
-                }
-                catch (IllegalArgumentException e) {
 
-                    if (e.toString().startsWith("java.lang.IllegalArgumentException: Message retrieval")) {
-                        /*
-                         * Messages >100 API limit error.
-                         */
-                        FunUtility.setEmbed(c.getEvent(), "❌ Selected Messages Are Out of Range ❌", "Only 1-100 messages can be deleted at a time.");
+                try {
+
+                    int num = Integer.parseInt(c.getCommandParameters().get(0));
+
+                    if(num > 0 && num <= 1000) {
+                        c.getChannel().getIterableHistory().takeAsync(num).thenAccept(messages -> {
+                            c.getChannel().purgeMessages(messages);
+                            FunUtility.setEmbed(c.getEvent(), "✅ Success! ✅", "You have successfully deleted " + messages.size() + " messages.");
+                        });
                     }
                     else {
-                        /*
-                         * Messages are too old error.
-                         */
-                        FunUtility.setEmbed(c.getEvent(), "❌ Selected Messages Are Older Than 2 Weeks ❌", "Messages older than 2 weeks cannot be deleted.");
+                        FunUtility.setEmbed(c.getEvent(), "❌ Invalid Argument ❌", "Please enter a number between 1 and 1000.");
                     }
+                }
+                catch(Exception e) {
+                    FunUtility.setEmbed(c.getEvent(), "❌ Invalid Argument ❌", "Please enter a number between 1 and 1000.");
                 }
             }
         }
@@ -58,6 +45,6 @@ public class Purge implements CommandInterface {
 
     @Override
     public String getName() {
-        return "purge";
+        return "delete";
     }
 }
