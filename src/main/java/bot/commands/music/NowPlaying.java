f12459ba937f0604e83538db4da78b4b08317a34
Rafi-99 <rafi.md.2018@gmail.com>
2021-01-22 00:18:36 -0600

    Added track progress feature

--- src/main/java/bot/commands/music/NowPlaying.java
+++ src/main/java/bot/commands/music/NowPlaying.java
@@ -2,6 +2,7 @@ package bot.commands.music;
 
 import bot.commands.CommandContext;
 import bot.commands.CommandInterface;
+import bot.handlers.event.FunUtility;
 import bot.handlers.music.MusicUtility;
 
 import java.util.Objects;
@@ -12,6 +13,18 @@ public class NowPlaying implements CommandInterface {
 
     @Override
     public void handle(CommandContext c) {
+        
+        long currentPositionMillis = MusicUtility.getMusicManager(c).player.getPlayingTrack().getPosition();
+        long totalDurationMillis = MusicUtility.getMusicManager(c).player.getPlayingTrack().getDuration();
+
+        long currentPositionMinutes = (currentPositionMillis/1000)/60;
+        long currentPositionSeconds = (currentPositionMillis/1000)%60;
+
+        long totalDurationMinutes = (totalDurationMillis/1000)/60;
+        long totalDurationSeconds = (totalDurationMillis/1000)%60;
+
+        String message = "Track Progress: "+ currentPositionMinutes +"m "+ currentPositionSeconds +"s/"+ totalDurationMinutes +"m "+ totalDurationSeconds +"s";
+
         if(c.getCommandParameters().isEmpty() && c.getMember().hasPermission(Permission.VOICE_CONNECT)) {
 
             if(Objects.requireNonNull(c.getMember().getVoiceState()).getChannel() != null && Objects.requireNonNull(c.getMember().getVoiceState()).getChannel() == MusicUtility.getAudioManager(c).getConnectedChannel() && MusicUtility.getAudioManager(c).isConnected()) {
@@ -21,8 +34,7 @@ public class NowPlaying implements CommandInterface {
                     c.getChannel().sendMessage("Nothing is being played currently.").queue();
                     return;
                 }
-                c.getChannel().sendTyping().queue();
-                c.getChannel().sendMessage("Currently playing: "+ MusicUtility.getMusicManager(c).player.getPlayingTrack().getInfo().title).queue();
+                FunUtility.setEmbed(c.getEvent(), "Currently Playing: "+ MusicUtility.getMusicManager(c).player.getPlayingTrack().getInfo().title, message);
             }
             else {
                 c.getChannel().sendTyping().queue();
