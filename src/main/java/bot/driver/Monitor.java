977da621641e5b1a7893ad516939bd337d518114
Rafi-99 <rafi.md.2018@gmail.com>
2021-03-04 13:39:57 -0600

    Changed to sharding implementation

--- src/main/java/bot/driver/Monitor.java
+++ src/main/java/bot/driver/Monitor.java
@@ -8,9 +8,9 @@ import java.sql.SQLException;
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
@@ -21,14 +21,12 @@ public class Monitor {
     public static void main(String[] args) throws LoginException, InterruptedException, SQLException, URISyntaxException {
         DataSource.getConnection();
 
-        myBot = JDABuilder.createDefault(System.getenv("BOT_TOKEN"))
+        DefaultShardManagerBuilder.createDefault(System.getenv("BOT_TOKEN"))
         .enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_VOICE_STATES)
         .setMemberCachePolicy(MemberCachePolicy.ALL.and(MemberCachePolicy.VOICE))
         .enableCache(CacheFlag.VOICE_STATE, CacheFlag.EMOTE)
         .addEventListeners(new EventListener())
-        .build()
-        .awaitReady();
-
-        myBot.getPresence().setActivity(Activity.listening("@[bot’s name]"));
+        .setActivity(Activity.listening("@[bot’s name]"))
+        .build();
     }
 }
