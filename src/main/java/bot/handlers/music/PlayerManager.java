e31245737c2657418037df1a6414184b1ff7b212
Rafi-99 <rafi.md.2018@gmail.com>
2021-03-20 17:48:35 -0500

    Reverted changes

--- src/main/java/bot/handlers/music/PlayerManager.java
+++ src/main/java/bot/handlers/music/PlayerManager.java
@@ -1,6 +1,5 @@
 package bot.handlers.music;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,62 +42,30 @@ public class PlayerManager {
                @Override
                public void trackLoaded(AudioTrack track) {
                     musicManager.scheduler.queue(track);
-
-                    if(trackUrl.contains("soundcloud")) {
-                         File logo = new File("src\\main\\java\\bot\\images\\SoundCloud.jpeg");
-                         EmbedBuilder player = new EmbedBuilder()
-                         .setColor(0x05055e)
-                         .setTitle(track.getInfo().title, track.getInfo().uri)
-                         .setThumbnail("attachment://SoundCloud.jpeg")
-                         .setDescription("Adding "+ track.getInfo().title +" to the queue.")
-                         .setFooter("The Monitor ™ | © 2021", "https://cdn.discordapp.com/avatars/711703852977487903/a7886964b1b5edab5c5d2eb5544a7da9.webp?size=512");
-                         channel.sendTyping().queue();
-                         channel.sendFile(logo, logo.getName()).embed(player.build()).queue();
-                         player.clear();
-                    }
-
-                    else {
-                         EmbedBuilder player = new EmbedBuilder()
-                         .setColor(0x05055e)
-                         .setTitle(track.getInfo().title, track.getInfo().uri)
-                         .setThumbnail("https://img.youtube.com/vi/"+ track.getInfo().identifier +"/default.jpg")
-                         .setDescription("Adding "+ track.getInfo().title +" to the queue.")
-                         .setFooter("The Monitor ™ | © 2021", "https://cdn.discordapp.com/avatars/711703852977487903/a7886964b1b5edab5c5d2eb5544a7da9.webp?size=512");
-                         channel.sendTyping().queue();
-                         channel.sendMessage(player.build()).queue();
-                         player.clear();
-                    }
+                    EmbedBuilder player = new EmbedBuilder()
+                    .setColor(0x05055e)
+                    .setTitle(track.getInfo().title, track.getInfo().uri)
+                    .setThumbnail("https://img.youtube.com/vi/"+ track.getInfo().identifier +"/default.jpg")
+                    .setDescription("Adding "+ track.getInfo().title +" to the queue.")
+                    .setFooter("The Monitor ™ | © 2021", "https://cdn.discordapp.com/avatars/711703852977487903/a7886964b1b5edab5c5d2eb5544a7da9.webp?size=512");
+                    channel.sendTyping().queue();
+                    channel.sendMessage(player.build()).queue();
+                    player.clear();    
                }
                @Override
                public void playlistLoaded(AudioPlaylist playlist) {
                     for (int i = 0; i < playlist.getTracks().size(); i++) {
                          musicManager.scheduler.queue(playlist.getTracks().get(i));
                     }
-
-                    if (trackUrl.contains("soundcloud")) {
-                         File logo = new File("src\\main\\java\\bot\\images\\SoundCloud.jpeg");
-                         EmbedBuilder player = new EmbedBuilder()
-                         .setColor(0x05055e)
-                         .setTitle(playlist.getName(), trackUrl)
-                         .setThumbnail("attachment://SoundCloud.jpeg")
-                         .setDescription("Adding " + playlist.getTracks().size() + " tracks from "+ playlist.getName() +" to the queue.")
-                         .setFooter("The Monitor ™ | © 2021", "https://cdn.discordapp.com/avatars/711703852977487903/a7886964b1b5edab5c5d2eb5544a7da9.webp?size=512");
-                         channel.sendTyping().queue();
-                         channel.sendFile(logo, logo.getName()).embed(player.build()).queue();
-                         player.clear();
-                    }
-
-                    else {
-                         EmbedBuilder player = new EmbedBuilder()
-                         .setColor(0x05055e)
-                         .setTitle(playlist.getName(), trackUrl)
-                         .setThumbnail("https://img.youtube.com/vi/"+ playlist.getTracks().get(0).getInfo().identifier +"/default.jpg")
-                         .setDescription("Adding " + playlist.getTracks().size() + " tracks from "+ playlist.getName() +" to the queue.")
-                         .setFooter("The Monitor ™ | © 2021", "https://cdn.discordapp.com/avatars/711703852977487903/a7886964b1b5edab5c5d2eb5544a7da9.webp?size=512");
-                         channel.sendTyping().queue();
-                         channel.sendMessage(player.build()).queue();
-                         player.clear();
-                    }
+                    EmbedBuilder player = new EmbedBuilder()
+                    .setColor(0x05055e)
+                    .setTitle(playlist.getName(), trackUrl)
+                    .setThumbnail("https://img.youtube.com/vi/"+ playlist.getTracks().get(0).getInfo().identifier +"/default.jpg")
+                    .setDescription("Adding " + playlist.getTracks().size() + " tracks from "+ playlist.getName() +" to the queue.")
+                    .setFooter("The Monitor ™ | © 2021", "https://cdn.discordapp.com/avatars/711703852977487903/a7886964b1b5edab5c5d2eb5544a7da9.webp?size=512");
+                    channel.sendTyping().queue();
+                    channel.sendMessage(player.build()).queue();
+                    player.clear();
                }
                @Override
                public void noMatches() {
